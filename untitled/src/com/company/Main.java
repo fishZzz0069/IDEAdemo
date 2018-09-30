151b90803da3261bc9fc2fc6e8e7aae01317d26b
zsy <shuangyuzhu0069@gmail.com>
2018-09-30 22:30:44 +0800

    Idea test

---
+++ untitled/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.print("HELLO NOWCODER");
+        System.out.print("HELLO NOWCODER2");
+        System.out.print("HELLO NOWCODER3");
+    }
+}
